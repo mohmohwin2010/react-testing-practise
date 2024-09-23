588439f93f2f458151cc453507eb202c118134f4
mohmohwin2010 <mohmohwin2010@gmail.com>
2024-09-23 07:02:08 +0630

    Exercise: Testing ExpandableText

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,9 +1,14 @@
+import ExpandableText from "../components/ExpandableText";
 import Onboarding from "../components/Onboarding";
 import TermsAndConditions from "../components/TermsAndConditions";
 
 const PlaygroundPage = () => {
   // return <Onboarding />;
-  return <TermsAndConditions />;
+  // return <TermsAndConditions />;
+
+  return (
+    <ExpandableText text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo minima saepe distinctio provident, sint, error assumenda omnis repellendus totam corrupti perferendis maiores laborum quo vitae ex nobis voluptates blanditiis nesciunt iusto impedit suscipit aperiam odio commodi aut. Quo, debitis! Cum aliquid dignissimos nihil dolorum molestiae odio non quasi architecto quisquam! Doloremque, alias unde adipisci architecto quaerat blanditiis consectetur aspernatur eius vitae laudantium, provident optio repudiandae nulla, dignissimos suscipit? Veniam suscipit ab tempora odio et aperiam quidem sapiente cum? Nulla, nobis. Aliquam delectus incidunt maiores, omnis quis excepturi culpa labore temporibus, neque accusantium reprehenderit aliquid amet libero vel soluta dolorem odio!" />
+  );
 };
 
 export default PlaygroundPage;
